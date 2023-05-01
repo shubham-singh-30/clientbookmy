f95dc4facfb26dd90ac08b22a8865e351ad3739b
shubham <shubham.cc@outlook.com>
2023-05-01 23:54:50 +0530

    monorepo

---
+++ src/components/Admin.js
@@ -0,0 +1,17 @@
+import React, { useEffect, useState } from 'react'
+import axios from "axios"
+let URL="http://localhost:6000"
+export const Admin = () => {
+    const [cities,setCities]= useState([])
+    useEffect(() => {
+        // Fetch the list of cities
+        axios.get('/api/cities')
+          .then(res => {
+            setCities(res.data);
+          })
+          .catch(err => console.error('Error getting cities:', err));
+      }, []);
+  return (
+    <div>Admin</div>
+  )
+}
